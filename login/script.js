07c4df757ae0c12ea97f0596797f91e080dff0f0
Gabriel Trindade Piber <66225099+AnorakG@users.noreply.github.com>
2021-12-01 16:38:23 -0300

    Update script.js

--- login/script.js
+++ login/script.js
@@ -14,9 +14,18 @@ let dataNascimento = document.getElementById('dataNascimento')
 
 let idade = 0
 localStorage.setItem("users", JSON.stringify(users))
+let contaConectada
+let usuarios 
 
 function cadastramento(){
+    anuncioCadastro.style.display = "none";
     formCadastro.style.display = "inline";
+    senhaValidez.style.display = "none";
+    nome.value=""
+    email.value=""
+    senha1.value=""
+    senha2.value=""
+    dataNascimento.value=""
 }
 function validarSenha(){
     if(senha1.value == senha2.value){
@@ -40,40 +49,72 @@ function confirmarIdade(){
 
 function cadastrar(){
     console.log("cadastrar");
-    if(idade >= 18){
+    console.log(users);
+    if(idade < 18){
+        anuncioCadastro.style.display="block";
+        anuncioCadastro.style.color = "red";
+        anuncioCadastro.innerHTML = "Menores de Idade não podem se cadastrar.";
+    }else if(users.find(confirmarNome)){
+        anuncioCadastro.style.display="block";
+        anuncioCadastro.style.color = "red";
+        anuncioCadastro.innerHTML = "Nome de usuário já cadastrado <br> Tente outro";
+    }else if(users.find(confirmarEmail)){
+        anuncioCadastro.style.display="block";
+        anuncioCadastro.style.color = "red";
+        anuncioCadastro.innerHTML = "E-mail já cadastrado <br> Esqueceu sua senha?";
+    }else{
         users.push({nome: nome.value, senha: senha2.value, email: email.value, idade: idade})
         localStorage.setItem("users", JSON.stringify(users))
         console.log(localStorage.getItem('users'))
         console.log("Cadastro feito =)")
+        anuncioCadastro.style.display= "inline";
         anuncioCadastro.style.color = "green";
         anuncioCadastro.innerHTML = "Parabéns por se cadastrar! <br> Cadastro realizado =)"
         formCadastro.style.display = "none";
-    }else{
-        anuncioCadastro.style.color = "red";
-        anuncioCadastro.innerHTML = "Menores de Idade não podem se cadastrar";
     }
+}
+
+function confirmarNome(users){
+    return users.nome === nome.value
+}
+function confirmarEmail(users){
+    return users.email === email.value
 }
 
 function buscarNome(usuarios){
     return usuarios.nome === login.value;
-}
-function buscarSenha(usuarios){
-    return usuarios.senha === senhaUsuario.value;
 }
 function buscarEmail(usuarios){
     return usuarios.email === login.value;
 }
 
 function confirmar(){
-    let usuarios = JSON.parse(localStorage.getItem('users'))
+    usuarios = JSON.parse(localStorage.getItem('users'))
     console.log (usuarios)
 
-    if(usuarios.find(buscarNome) && usuarios.find(buscarSenha)){
-        alert("Você está logado")
-    }else if(usuarios.find(buscarEmail) && usuarios.find(buscarSenha)){
-        alert("Você está logado")
+    if(usuarios.find(buscarNome)){
+        console.log('Nome encontrado!')
+        console.log(usuarios.find(buscarNome))
+        contaConectada = usuarios.find(buscarNome)
+        confirmarSenha()
+    }else if(usuarios.find(buscarEmail)){
+        console.log('E-mail encontrado!')
+        console.log(usuarios.find(buscarEmail))
+        contaConectada = usuarios.find(buscarEmail)
+        confirmarSenha()
     }else{
         alert("Cadastre-se por favor")
-        formCadastro.style.display = "inline"; 
+        anuncioCadastro.style.display = "none"
+        formCadastro.style.display = "inline";        
+    }
+}
+
+function confirmarSenha(){
+    if(contaConectada.senha === senhaUsuario.value){
+        alert('Você está logado')
+        senhaUsuario.value = ""
+        login.value = ""
+    }else{
+        alert('Senha ou usuário incorretas \nTente novamente ou cadastre-se por favor')
     }
 }
