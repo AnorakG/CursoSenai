81a698afcba4ed76d052d137753831cb6826ff01
Gabriel Trindade Piber <66225099+AnorakG@users.noreply.github.com>
2022-02-07 01:02:59 -0300

    Update exercicio3.js

--- exercicios lucas/exercicio3.js
+++ exercicios lucas/exercicio3.js
@@ -1,12 +1,14 @@
 let numeros = [1,3,4,6,9]
-let value2=0
+
 function impares(value) {
     if(value%2 != 0){
         return value
     }
-    for(let i = 0;i<5;i++){
-        value2 += value[i]  
-    }
-    console.log(value2)
 }
-console.log(numeros.filter(impares))
+
+let impar = numeros.filter(impares)
+let somatoria 
+
+const soma = (valor1, valor2) => valor1 + valor2
+console.log(impar)
+console.log(impar.reduce(soma))
